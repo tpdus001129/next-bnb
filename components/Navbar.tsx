fb0d1e444e06f5a7d431ddf4ddcc321985d5d0e7
kimseyeon <tpdus001129@gmail.com>
2025-01-02 11:48:13 +0900

    8-6. 사용자 정보 수정 폼

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
+
 import { useState } from "react"
 import { useRouter } from "next/navigation"
+
 import { MdModeOfTravel } from "react-icons/md"
 import { RxDividerVertical } from "react-icons/rx"
 import { AiOutlineSearch } from "react-icons/ai"
+
 import { AiOutlineMenu } from "react-icons/ai"
 import { AiOutlineUser } from "react-icons/ai"
 
 import cn from "classnames"
+
 import Link from "next/link"
 import { SearchFilter } from "./Filter"
 import { useRecoilState, useRecoilValue } from "recoil"
 import { detailFilterState, filterState } from "@/atom"
 import { signOut, useSession } from "next-auth/react"
 
-const LOGIN_USER_MENU = [
+const LOGOUT_USER_MENU = [
   { id: 1, title: "로그인", url: "/users/signin" },
   { id: 2, title: "회원가입", url: "/users/signin" },
   { id: 3, title: "FAQ", url: "/faqs" },
 ]
 
-const LOGOUT_USER_MENU = [
-  { id: 1, title: "로그아웃", url: "#", signOut: true },
+const LOGIN_USER_MENU = [
+  { id: 1, title: "마이페이지", url: "/users/mypage" },
   { id: 2, title: "FAQ", url: "/faqs" },
+  { id: 3, title: "로그아웃", url: "#", signOut: true },
 ]
 
 export default function Navbar() {
@@ -208,7 +213,7 @@ export default function Navbar() {
         {showMenu && (
           <div className="border border-gray-200 shadow-lg py-2 flex flex-col absolute top-12 bg-white w-60 rounded-lg">
             {status === "unauthenticated"
-              ? LOGIN_USER_MENU?.map((menu) => (
+              ? LOGOUT_USER_MENU?.map((menu) => (
                   <button
                     type="button"
                     key={menu.id}
@@ -221,7 +226,7 @@ export default function Navbar() {
                     {menu.title}
                   </button>
                 ))
-              : LOGOUT_USER_MENU?.map((menu) => (
+              : LOGIN_USER_MENU?.map((menu) => (
                   <button
                     type="button"
                     key={menu.id}
--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,6 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/mypage"] }
+// 로그인 된 사용자만 들어오게~
+export const config = {
+  matcher: ["/users/mypage", "/users/info", "/users/edit"],
+}
